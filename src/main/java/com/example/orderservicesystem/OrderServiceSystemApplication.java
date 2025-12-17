361e214f22b4a872db9c8d3642b63a50106a46a2
suryans77 <suryansdash005@gmail.com>
2025-12-17 20:07:21 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/orderservicesystem/OrderServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderservicesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceSystemApplication.class, args);
+    }
+
+}
